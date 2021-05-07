22d4cb8eefa8838a1f4c825844f35d7db10f62d9
AlessandroAmenduni <alessandro.amenduni2934@gmail.com>
2021-05-07 15:17:58 +0200

    Add axios post

--- src/Components/registrazione.js
+++ src/Components/registrazione.js
@@ -5,33 +5,47 @@ import reactDom from 'react-dom';
 
 
 function Registrazione() {
+
+
   
   return (
     <div class="form-container">
-    <form action="/login" id="form" method="POST">
+      <div id="reg">
       <h3>Registrazione</h3>
       <class class="container">
         <span class="icon">
           <i class="fasfa-user"></i>
         </span>
-        <input type="text" placeholder="Username"/>
+        <input type="text" placeholder="Username" id="username"/>
         <div class="container">
           <span class="icon">
             <i class="fasfa-at"></i>
           </span>
-          <input type="email" placeholder="email"/>
+          <input type="email" placeholder="email" id="email"/>
         </div>
         <div class="container">
           <span class="icon">
             <i class="fasfa-lock"></i>
           </span>
-          <input type="password" placeholder="password"/>
+          <input type="password" placeholder="password" id="password"/>
         </div>
-        <input type="submit" value="registrati" id="registrazione"/>
+        <input type="submit" value="registrati" id="registrazione" onClick={Redirect}/>
       </class>
-    </form>
-  </div>
+      </div>
+    </div>
   );
+
+  function Redirect(){
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/register',
+      data: {
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+      }
+    });
+  }
 }
 
 export default Registrazione;
